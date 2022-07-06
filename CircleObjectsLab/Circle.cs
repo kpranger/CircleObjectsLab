37aa5be7d6ff5ce54fbf839c9e95217dcb59e53d
Kris <pranger.kris@gmail.com>
2022-07-06 08:29:57 -0400

    Circle Objects Lab

---
+++ CircleObjectsLab/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsLab
+{
+    internal class Circle
+    {
+        //Properties
+        public double Radius;
+
+        //Constructor
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+
+        //Methods
+        public double CalculateCircumference()
+        {
+            double circumference = (2 * Math.PI * Radius);
+            return circumference;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            string formattedCircumference = FormatNumber(CalculateCircumference());
+            return formattedCircumference;
+        }
+        public double CalculateArea()
+        {
+            double area = (Math.PI * (Radius * Radius));
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+        }
+    }
+}
---
+++ CircleObjectsLab/Program.cs
@@ -0,0 +1,35 @@
+﻿using CircleObjectsLab;
+
+bool runProgram = true;
+while (runProgram)
+{
+
+    double radius = 0;
+    string area = "";
+    string circumference = "";
+
+    Console.WriteLine("Please enter the radius of a circle:");
+    radius = double.Parse(Console.ReadLine());
+
+    Circle myCircle = new Circle(radius);
+
+    area = myCircle.CalculateFormattedArea();
+    circumference = myCircle.CalculateFormattedCircumference();
+
+    Console.WriteLine(area);
+    Console.WriteLine(circumference);
+    while (true)
+    {
+        Console.WriteLine("Would you like to find the radius and circumference of another number? y/n");
+        string choice = Console.ReadLine();
+        if (choice.ToLower().Trim() == "y")
+        {
+            break;
+        }
+        else if (choice.ToLower().Trim() == "n")
+        {
+            runProgram = false;
+            break;
+        }
+    }
+}
